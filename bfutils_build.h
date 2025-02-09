01964871a5bd2c9e5a33afb5e68ac466f31dbad4
Bruno Flavio Ferreira <ndninguem9@gmail.com>
2025-02-09 17:33:35 -0300

    Fix copyright date

--- bfutils_build.h
+++ bfutils_build.h
@@ -60,7 +60,7 @@ LICENSE:
 
     MIT License
     
-    Copyright (c) 2024 Bruno Flávio Ferreira 
+    Copyright (c) 2025 Bruno Flávio Ferreira 
     
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
